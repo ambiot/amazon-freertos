6358afdafb3717b471976b8b6fa7cc9b76ea4db7
Prashant Ravi <prashantrar@gmail.com>
2019-06-25 11:08:06 +0800

    adding lwip portable files for realtek

---
+++ lib/third_party/lwip/src/portable/realtek/lwip_def.h
@@ -0,0 +1,37 @@
+#ifndef LWIP_DEF_H
+#define LWIP_DEF_H
+
+#include "lwip/opt.h"
+#include "lwip/def.h"
+
+#include "lwip/ip4_addr.h"
+#include "lwip/ip6_addr.h"
+
+#if !NO_SYS
+#include "lwip/sys.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if LWIP_IPV4
+/* Added by Realtek start */
+#define ip_addr ip4_addr
+/* Added by Realtek end */
+#else /* LWIP_IPV4 */
+/* Added by Realtek start */
+#define ip_addr ip6_addr
+/* Added by Realtek end */
+#endif
+
+
+//Realtek add 
+struct sys_timeouts {
+  struct sys_timeo *next;
+};
+
+struct sys_timeouts *sys_arch_timeouts(void);
+//Realtek add end
+
+#endif
