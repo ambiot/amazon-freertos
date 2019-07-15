e74e1b5c87949bea32e51499b70d5ed6bd6c1ef6
Prashant Ravi <prashantrar@gmail.com>
2019-07-15 20:11:32 +0800

    deleting unnecessary files, trimming sdk

--- lib/third_party/mcu_vendor/realtek/component/common/api/at_cmd/atcmd_bt.h
+++
@@ -1,5 +0,0 @@
-#ifndef __ATCMD_BT_H__
-#define __ATCMD_BT_H__
-
-#endif  /* __ATCMD_BT_H__ */
-
--- lib/third_party/mcu_vendor/realtek/component/common/example/trust_zone/example_trust_zone.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMPLE_TRUST_ZONE_H
-#define EXAMPLE_TRUST_ZONE_H
-
-void example_trust_zone(void);
-
-#endif /* EXAMPLE_TRUST_ZONE_H */
