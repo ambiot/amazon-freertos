1a756e723ba2481b09dd8ec6708002a2e689d41c
hank.su <hank.su@realtek.com>
2020-08-18 11:11:24 +0800

    Remove ambz2-v1.4.7
    Please use ambz2-v1.4.7 branch if needed.

--- Hardware Information/sample_device_tester_configs/build.bat
+++
@@ -1,7 +0,0 @@
-set IARDir="E:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1"
-set IARProjectDir=%1%\tests\realtek\amebaz2\iar
-set PorjectName=application_is
-
-
-#%IARDir%\common\bin\IarBuild.exe %IARProjectDir%\%PorjectName%.ewp -clean Debug
-%IARDir%\common\bin\IarBuild.exe %IARProjectDir%\%PorjectName%.ewp -make Debug
--- lib/third_party/mcu_vendor/realtek/component/common/example/amazon_freertos/example_amazon_freertos.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMPLE_AMAZON_FREERTOS_H
-#define EXAMPLE_AMAZON_FREERTOS_H
-
-void example_amazon_freertos(void);
-
-#endif /* EXAMPLE_AMAZON_FREERTOS_H */
